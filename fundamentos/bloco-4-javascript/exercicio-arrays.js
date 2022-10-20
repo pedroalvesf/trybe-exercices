7688202a757896e54f355eb9afe18f5bf4804711
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-20 16:15:10 -0300

    finalizando questao bonus

--- fundamentos/bloco-4-javascript/exercicio-arrays.js
+++ fundamentos/bloco-4-javascript/exercicio-arrays.js
@@ -68,3 +68,20 @@
 //     // console.log(a)
 //     // console.log(a/2)
 // }
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers2 = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    if(i + 1 < numbers.length) {
+        numbers2.push(numbers[i]* numbers[i+1])
+    }else {
+        numbers2.push(numbers[i] * 2);
+    }
+}
+
+
+console.log(numbers2)
+
+console.log(numbers)
+console.log(numbers.sort(function(a, b){return a- b}))
