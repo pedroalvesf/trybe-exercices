a0a75e2854c9024bae8e5412ed34d63c581f8252
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-20 19:22:20 -0300

    segundo exercicio de fixando pronto

--- fundamentos/bloco-4-javascript/exercicio-logica.js
+++ fundamentos/bloco-4-javascript/exercicio-logica.js
@@ -19,11 +19,47 @@
 //   resultado -= index;
 // };
 // console.log(resultado);
-let sum = 0;
+// let sum = 0;
 
-for(let i = 0; i <=100; i++) { 
+// for(let i = 0; i <=100; i++) { 
 
-    sum += i; 
+//     sum += i; 
+// }
+
+// console.log(sum)
+//
+
+
+
+//Questao de fatorial
+// let num = 10;
+// let soma = 0;
+// for (let i = 0; i < num; i++){
+//     soma += num*i  
+// }
+
+// console.log(soma)
+
+let word = 'tryber'
+let word2='';
+// let word2 = ;
+
+// for(let i = 0; i <= word.length; i++){
+
+// }
+function invert(){
+    return word.split('').reverse().join('')
 }
 
-console.log(sum)
+
+
+for(let i = word.length -1; i >= 0; i--){
+    console.log(word[i])
+    word2 += word[i];
+
+}
+console.log("Using FOR loop above but with a wrong result")
+console.log(invert(word))
+console.log("Using function method above")
+console.log(word2)
+console.log("Using FOR loop above")
