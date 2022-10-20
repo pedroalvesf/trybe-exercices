a3052609413acb187edc7d4430625ad526109fcf
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-20 18:42:56 -0300

    quase finalizando dia 3 de js

--- fundamentos/bloco-4-javascript/exercicio-logica.js
+++ fundamentos/bloco-4-javascript/exercicio-logica.js
@@ -12,10 +12,18 @@
 //   console.log('valor menor que 16');
 // }
 
-const n =  ;
+// const n =  4;
 
-let resultado = 100;
-for (let index = 0; index <= n; index += 1) {
-  resultado -= index;
-};
-console.log(resultado);
+// let resultado = 100;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado -= index;
+// };
+// console.log(resultado);
+let sum = 0;
+
+for(let i = 0; i <=100; i++) { 
+
+    sum += i; 
+}
+
+console.log(sum)
