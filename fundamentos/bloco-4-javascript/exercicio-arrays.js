f8926b7f0ab5537659d9ceb56e1ce196bdda2a50
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-20 13:16:43 -0300

    ajustando atividades

--- fundamentos/bloco-4-javascript/exercicio-arrays.js
+++ fundamentos/bloco-4-javascript/exercicio-arrays.js
@@ -1,6 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
-let media = 0;
+let average = 0;
 let higher = numbers[0]
 
 for (let i = 0; i < numbers.length; i+= 1) {
@@ -8,17 +8,29 @@ for (let i = 0; i < numbers.length; i+= 1) {
         higher = numbers[i];
     }
      sum += numbers[i]; 
-     media = sum / numbers.length
+     average = sum / numbers.length
 }
 
 function higherNumber(){
     return Math.max(...numbers)
 
 }
+console.log("-------------------------------------")
 
+let pairList = [];
+let Evenlist = [];
+for (let index = 0; index < numbers.length; index+= 1) {
+    if(numbers[index] % 2 == 0){
+        pairList.push(numbers[index]);
+    }else{
+        Evenlist.push(numbers[index]);
+    }
+}
 
-console.log("A soma da: " + sum);
-console.log("A media da: " + media);
-console.log("Maior numero: " + higher);
-console.log("Maior numero: " + Math.max(...numbers))
-console.log("Maior numero: " + higherNumber())
+console.log("Pair list: " + pairList) //line 20 - 28 
+console.log("Even list: " + Evenlist)
+console.log("The summup is: " + sum);
+console.log("The average is: " + average); //line 6 a 13
+console.log("Biggest number: " + higher);
+console.log("Biggest number: " + Math.max(...numbers))//Using function direct
+console.log("Biggest number: " + higherNumber())//line 14 e 15
