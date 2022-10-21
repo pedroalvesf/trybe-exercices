a62dd139cf7dafe1f8fba7369f87ee8bd65f04e1
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-21 19:51:05 -0300

    finalizando estudos por hoje dia 21

--- fundamentos/bloco-4-javascript/exercicio-arrays-prat.js
+++ fundamentos/bloco-4-javascript/exercicio-arrays-prat.js
@@ -1,23 +1,43 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 
-function organizaLista() {
-    return numbers.sort(function (a, b) { return a - b })
-}
+// function organizaLista() {
+//     return numbers.sort(function (a, b) { return a - b })
+// }
 
-console.log(organizaLista())
+// console.log(organizaLista())
 
-let numbers1 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let numbers2 = [];
+// let numbers1 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers2 = [];
 
-console.log(numbers1.length)
+// console.log(numbers1.length)
 
-for (let i = 0; i < numbers1.length; i++) {
-    if (i + 1 < numbers1.length) {
-        numbers2.push(numbers1[i] * numbers1[i + 1])
-    } else {
-        numbers2.push(numbers1[i] * 2)
+// for (let i = 0; i < numbers1.length; i++) {
+//     if (i + 1 < numbers1.length) {
+//         numbers2.push(numbers1[i] * numbers1[i + 1])
+//     } else {
+//         numbers2.push(numbers1[i] * 2)
+//     }
+// }
+
+// console.log(numbers2)
+
+
+// console.log("--------------------------")
+
+
+let list = ['java', 'javascript', 'python', 'html', 'css'];
+
+let big =list[0];
+let sml =list[0];
+
+for(let i = 0; i < list.length; i++){
+    if(list[i].length > big.length ){
+        big = list[i]
+    }if(list[i].length < sml.length){
+        sml = list[i]
     }
 }
 
-console.log(numbers2)
+console.log(big)
+console.log(sml)
