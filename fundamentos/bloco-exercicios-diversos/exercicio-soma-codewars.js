f46d2a1d885d59f78dc8a2a2991350b20d1a3820
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-24 12:24:58 -0300

    questao codewars

---
+++ fundamentos/bloco-exercicios-diversos/exercicio-soma-codewars.js
@@ -0,0 +1,10 @@
+//somar os elementos do numero que for n
+//somar novamente caso a primeira soma seja maior do que 2 digitos, por exemplo =/ n=99  //  9 +_9 = 18 //  1 + 8 =9
+
+
+let n = 16;
+let soma = 0;
+
+console.log(n)
+console.log(n.length)
+
