d94831c2b8fb4f2c884fc2246affa042808aa502
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-25 12:12:37 -0300

    ajustando questao codewars

--- fundamentos/bloco-exercicios-diversos/exercicio-soma-codewars.js
+++ fundamentos/bloco-exercicios-diversos/exercicio-soma-codewars.js
@@ -1,7 +1,6 @@
 //somar os elementos do numero que for n
 //somar novamente caso a primeira soma seja maior do que 2 digitos, por exemplo =/ n=99  //  9 +_9 = 18 //  1 + 8 =9
 
-
 let n = 99;
 let soma = 0;
 let soma2 = 0;
@@ -19,21 +18,17 @@ for (let i = 0; i < n2.length; i++) {
     nList.push(n2[i])
     // console.log(n2[i])
     soma += parseInt(nList[i])
-
 }
-
 console.log("Sumup: " + soma)
 console.log(nList)
 
 let n3 = soma.toFixed(0);
-
 if (n3.length >= 2) {
     for (let o = 0; o < n3.length; o++) {
         nList2.push(n3[o])
         soma2 += parseInt(n3[o])
     }
 }
-
 console.log("Sumup2 : " + soma2)
 console.log(nList2)
 
