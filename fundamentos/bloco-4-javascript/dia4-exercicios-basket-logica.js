8967cd1901ee6c2ca75fc30aa50cc225f0c5b75f
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-26 11:53:41 -0300

    questao funcao basket

--- fundamentos/bloco-4-javascript/dia4-exercicios-basket-logica.js
+++ fundamentos/bloco-4-javascript/dia4-exercicios-basket-logica.js
@@ -1,91 +1,123 @@
 const basket = [
-    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
-    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
-    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
-    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
-    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
-    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
-    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
-    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
-    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
-    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
-    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
-    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
-    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
-    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
-    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
-    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
-    'Banana', 'Pera', 'Abacate', 'Uva',
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
 ];
- let fruits = {
-    cMelancia: [],
-    cAbacate : [],
-    cUva : [],
-    cJaca : [],
-    cLaranja : [],
-    cBanana : [],
-    cPera : []
+//  let fruits = {
+//     cMelancia: [],
+//     cAbacate : [],
+//     cUva : [],
+//     cJaca : [],
+//     cLaranja : [],
+//     cBanana : [],
+//     cPera : []
+// }
+
+function countFruits2(basket) {
+  let fruitVariable;
+  let fruits = {
+    Melancia: 0,
+    Abacate: 0,  
+    Uva: 0,
+    Jaca: 0,
+    Laranja: 0,
+    Banana: 0,
+    Pera: 0
+  }
+  for (let i in basket) {
+    fruitVariable = fruitBasket(basket[i], fruits)
+  }
+  return fruitVariable;
 }
 
-function countFruits(basket) {
-    for (let i in basket) {
-        if (basket[i] == 'Melancia') {
-            fruits.cMelancia.push(basket[i])
-        }
-        if (basket[i] == 'Abacate') {
-            fruits.cAbacate.push(basket[i])
-        }
-        if (basket[i] == 'Uva') {
-            fruits.cUva.push(basket[i])
-        }
-        if (basket[i] == 'Jaca') {
-            fruits.cJaca.push(basket[i])
-        }
-        if (basket[i] == 'Laranja') {
-            fruits.cLaranja.push(basket[i])
-        }
-        if (basket[i] == 'Banana') {
-            fruits.cBanana.push(basket[i])
-        }
-        if (basket[i] == 'Pera') {
-            fruits.cPera.push(basket[i])
-        }
+function fruitBasket(frutas, objeto) {
+  objeto[frutas] += 1;
 
-    }
-    return fruits;
+  return objeto;
+}
+let funcao = countFruits2(basket) //To call the function you must use a variable; 
 
-  }
-console.log("----------------------------")
-console.log("----------------------------")
-  const strFrequency = function (fruta) {
-    return fruta.reduce((count, word) => {
-          count[word] = (count[word] || 0) + 1;
-          return count;
-    }, {})
-  }
-console.log(strFrequency(basket));
-console.log("----------------------------")
-console.log("----------------------------")
+console.log(countFruits2(basket))
+console.log("Melancia: " + funcao.Melancia)
 
-console.log(fruits)
-console.log("----------------------------")
-console.log(countFruits(basket))
-console.log("Melancias: " + fruits.cMelancia.length)
-console.log("Abacate: " + fruits.cAbacate.length)
-console.log("Uva: " + fruits.cUva.length)
-console.log("Jaca: " + fruits.cJaca.length)
-console.log("Laranja: " + fruits.cLaranja.length)
-console.log("Banana: " + fruits.cBanana.length)
-console.log("Pera: " + fruits.cPera.length)
-console.log("Abacate: " + fruits.cAbacate.length)
+// function countFruits(basket) {
+//   for (let i in basket) {
+//     if (basket[i] == 'Melancia') {
+//       fruits.cMelancia.push(basket[i])
+//     }
+//     if (basket[i] == 'Abacate') {
+//       fruits.cAbacate.push(basket[i])
+//     }
+//     if (basket[i] == 'Uva') {
+//       fruits.cUva.push(basket[i])
+//     }
+//     if (basket[i] == 'Jaca') {
+//       fruits.cJaca.push(basket[i])
+//     }
+//     if (basket[i] == 'Laranja') {
+//       fruits.cLaranja.push(basket[i])
+//     }
+//     if (basket[i] == 'Banana') {
+//       fruits.cBanana.push(basket[i])
+//     }
+//     if (basket[i] == 'Pera') {
+//       fruits.cPera.push(basket[i])
+//     }
+
+//   }
+//   return fruits;
+
+// }
+// console.log("----------------------------")
+// console.log("----------------------------")
+//   const strFrequency = function (fruta) {
+//     return fruta.reduce((count, word) => {
+//           count[word] = (count[word] || 0) + 1;
+//           return count;
+//     }, {})
+//   }
+// console.log(strFrequency(basket));
+// console.log("----------------------------")
+// console.log("----------------------------")
+
+// console.log(fruits)
+// console.log("----------------------------")
+// console.log(countFruits(basket))
+// console.log("Melancias: " + fruits.cMelancia.length)
+// console.log("Abacate: " + fruits.cAbacate.length)
+// console.log("Uva: " + fruits.cUva.length)
+// console.log("Jaca: " + fruits.cJaca.length)
+// console.log("Laranja: " + fruits.cLaranja.length)
+// console.log("Banana: " + fruits.cBanana.length)
+// console.log("Pera: " + fruits.cPera.length)
+// console.log("Abacate: " + fruits.cAbacate.length)
 
 //Utilizar for dentro de for 
-for(let i = 0; i < basket.length; i++){
-    //utilizar o primeiro contador
-    for(let o = 0; o < basket.length; o++){
-        // utilizar segundo contador
-    }
-}
+
+// let contaFrut = 0;
+// let conFrut = 0;
+// for (let i = 0; i < basket.length; i++) {
+//   let f = basket[i];
+//   for (let o = 0; o < basket.length; o++) {
+//     if (f == basket[o]) {
+//       contaFrut += 1;
+//     }
+//   }
+// }
 
 //resolucao do gabarito abaixo 
 
--- fundamentos/bloco-4-javascript/dia4-js-funcoes.js
+++ fundamentos/bloco-4-javascript/dia4-js-funcoes.js
@@ -1,176 +1,201 @@
-let a = 15;
-let b = 8;
+// let a = 15;
+// let b = 8;
 
 
 
-function add() {
-    return a + b;
-}
+// function add() {
+//     return a + b;
+// }
 
-function minus() {
-    return a - b;
-}
+// function minus() {
+//     return a - b;
+// }
 
-function multipl() {
-    return a * b;
-}
+// function multipl() {
+//     return a * b;
+// }
 
-function div() {
-    return a / b;
-}
+// function div() {
+//     return a / b;
+// }
 
-function modulo() {
-    return a % b;
-}
+// function modulo() {
+//     return a % b;
+// }
 
-function maiorQ() {
-    if (a > b) {
-        return "A is bigger"
-    } else {
-        return "B is bigger"
-    }
-}
-let n = 0;
-function define() {
-    if (n > 0) {
-        return "positive"
-    } else if (n < 0) {
-        return "negative"
-    } else {
-        return "Equal to zero"
-    }
-}
-console.log(add(a, b))
-console.log(minus(a, b))
-console.log(multipl(a, b))
-console.log(div(a, b))
-console.log(modulo(a, b))
-console.log(maiorQ(a, b))
-console.log(define(n));
-console.log('------------------')
+// function maiorQ() {
+//     if (a > b) {
+//         return "A is bigger"
+//     } else {
+//         return "B is bigger"
+//     }
+// }
+// let n = 0;
+// function define() {
+//     if (n > 0) {
+//         return "positive"
+//     } else if (n < 0) {
+//         return "negative"
+//     } else {
+//         return "Equal to zero"
+//     }
+// }
+// console.log(add(a, b))
+// console.log(minus(a, b))
+// console.log(multipl(a, b))
+// console.log(div(a, b))
+// console.log(modulo(a, b))
+// console.log(maiorQ(a, b))
+// console.log(define(n));
+// console.log('------------------')
 
-let aA = 60;
-let aB = 60;
-let aC = 60;
+// let aA = 60;
+// let aB = 60;
+// let aC = 60;
 
-function triangleAngles(aA, aB, aC) {
-    let sum = aA + aB + aC;
-    let allAnglesPositivo = aA > 0 && aB > 0 && aC > 0;
+// function triangleAngles(aA, aB, aC) {
+//     let sum = aA + aB + aC;
+//     let allAnglesPositivo = aA > 0 && aB > 0 && aC > 0;
 
-    if (allAnglesPositivo) {
-        if (sum === 180) {
-            return true;
-        } else {
-            return false
-        }
-    } else {
-        return "Invalid Angle"
-    }
+//     if (allAnglesPositivo) {
+//         if (sum === 180) {
+//             return true;
+//         } else {
+//             return false
+//         }
+//     } else {
+//         return "Invalid Angle"
+//     }
 
-}
+// }
 
-console.log(triangleAngles(aA, aB, aC))
+// console.log(triangleAngles(aA, aB, aC))
 
-console.log("--------------------------------------");
+// console.log("--------------------------------------");
 
-console.log("Checking palindromo bellow")
-let palavra = "tatt"
-function checkPalin(palavra) {
-    let palavra2 = palavra.split('').reverse().join('')
+// console.log("Checking palindromo bellow")
+// let palavra = "tatt"
+// function checkPalin(palavra) {
+//     let palavra2 = palavra.split('').reverse().join('')
 
-    if (palavra2 === palavra) {
-        return true;
-    } else {
-        return false;
-    }
-}
-console.log(checkPalin(palavra))
-console.log("--------------------------------------");
+//     if (palavra2 === palavra) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(checkPalin(palavra))
+// console.log("--------------------------------------");
 
-console.log("Max and min value per index in array bellow")
-let array = [2, 3, 6, 7, 10, 1, -1];
+// console.log("Max and min value per index in array bellow")
+// let array = [2, 3, 6, 7, 10, 1, -1];
 
-function maxIndex() {
-    return array.indexOf(Math.max(...array));
-}
+// function maxIndex() {
+//     return array.indexOf(Math.max(...array));
+// }
 
-function minIndex() {
-    return array.indexOf(Math.min(...array))
-}
+// function minIndex() {
+//     return array.indexOf(Math.min(...array))
+// }
 
-console.log("Biggest number index: " + maxIndex(array))
-console.log("Smallest number index: " + minIndex(array))
+// console.log("Biggest number index: " + maxIndex(array))
+// console.log("Smallest number index: " + minIndex(array))
 
-console.log("--------------------------------------");
-console.log("Biggest and Smallest name bellow");
+// console.log("--------------------------------------");
+// console.log("Biggest and Smallest name bellow");
 
-let names = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+// let names = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
-function biggestName() {
-    let big = names[0];
-    for (let i in names) {
-        if (big.length < names[i].length) {
-            big = names[i];
-        }
-    }
-    return big;
-}
+// function biggestName() {
+//     let big = names[0];
+//     for (let i in names) {
+//         if (big.length < names[i].length) {
+//             big = names[i];
+//         }
+//     }
+//     return big;
+// }
 
-function smallestName(){
-    let sml = names[0];
-    for(let i in names){
-        if(sml.length > names[i].length){
-            sml = names[i];
-        }
-    }
-    return sml;
-}
+// function smallestName(){
+//     let sml = names[0];
+//     for(let i in names){
+//         if(sml.length > names[i].length){
+//             sml = names[i];
+//         }
+//     }
+//     return sml;
+// }
 
-console.log(biggestName(names));
-console.log(names.indexOf(biggestName(names)));
-console.log(smallestName(names));
-console.log(names.indexOf(smallestName(names)));
+// console.log(biggestName(names));
+// console.log(names.indexOf(biggestName(names)));
+// console.log(smallestName(names));
+// console.log(names.indexOf(smallestName(names)));
 
-console.log("--------------------------------------");
-const checkLength = Math.max(...names.map(element => element.length));
-console.log(checkLength)
+// console.log("--------------------------------------");
+// const checkLength = Math.max(...names.map(element => element.length));
+// console.log(checkLength)
 
-console.log("--------------------------------------");
-console.log("exe 5");
+// console.log("--------------------------------------");
+// console.log("exe 5");
+
+// let numbers = [2, 3, 2, 5, 8, 2, 3];
+// function repeatNumbers (numbers){
+//     let countRepeat = 0;
+//     let countNumber = 0;
+//     let indexRepeatedNumber =0;
+
+//     for(let i in numbers){
+//         let checkNumber = numbers[i];
+//         for (let i2 in numbers){
+//             if(checkNumber === numbers[i2]){
+//                 countNumber += 1;
+//             }
+//         }
+//         if(countNumber > countRepeat){
+//             countRepeat = countNumber;
+//             indexRepeatedNumber = i;
+//         }
+//         countNumber = 0;
+//     }
+//     return numbers[indexRepeatedNumber];
+// }
+
+// console.log(repeatNumbers(numbers));
+
 
 let numbers = [2, 3, 2, 5, 8, 2, 3];
-function repeatNumbers (numbers){
-    let countRepeat = 0;
-    let countNumber = 0;
-    let indexRepeatedNumber =0;
+function repeatedNumbers(numbers) {
 
-    for(let i in numbers){
-        let checkNumber = numbers[i];
-        for (let i2 in numbers){
-            if(checkNumber === numbers[i2]){
-                countNumber += 1;
+    let countN = 0;
+    let numbersArray = 0;
+    for (let i in numbers) {
+        let count = 0;
+        let number = numbers[i];
+        for (let i2 in numbers) {
+            if (number == numbers[i2]) {
+                count += 1;
+                
             }
         }
-        if(countNumber > countRepeat){
-            countRepeat = countNumber;
-            indexRepeatedNumber = i;
+        if(count > countN){
+            countN = count;
+            numbersArray = number;
         }
-        countNumber = 0;
     }
-    return numbers[indexRepeatedNumber];
+    return numbersArray + ' count: ' + countN
 }
 
-console.log(repeatNumbers(numbers));
+console.log(repeatedNumbers(numbers))
 
-console.log("--------------------------------------");
+// console.log("--------------------------------------");
 
-let eva = 50000000;
-function sumUp(eva){
-    let sum = 0;
-    for(let i = 0; i <= eva; i++){
-        sum += i; 
-    }
-    return sum;
-}
+// let eva = 50000000;
+// function sumUp(eva){
+//     let sum = 0;
+//     for(let i = 0; i <= eva; i++){
+//         sum += i; 
+//     }
+//     return sum;
+// }
 
-console.log(sumUp(eva))
+// console.log(sumUp(eva))
