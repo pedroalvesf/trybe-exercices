f2e0f167129394433528837db6e31610ad27f608
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-26 17:52:20 -0300

    finalizando questoes comum do dia 5

--- fundamentos/bloco-4-javascript/dia5-js-let-cons-arrow.js
+++ fundamentos/bloco-4-javascript/dia5-js-let-cons-arrow.js
@@ -28,5 +28,62 @@
 // const multiplication = (number, multi) => number * multi;
 // console.log(multiplication(8, 2));
 
-const soma = (a, b) => a + b;
-console.log(soma(1, 2))
+// const soma = (a, b) => a + b;
+// console.log(soma(1, 2))
+
+    // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+    //just to change const to let 
+    // let pessoa = {
+    //     nome: 'Henri',
+    //     idade: 20
+    //   }
+    //   pessoa = {
+    //     nome: 'Luna',
+    //     idade: 19
+    //   } // Altere essa estrutura para corrigir o erro.
+    //   console.log('Nome:', pessoa.nome);
+    //   console.log('Idade:', pessoa.idade);
+// commom way 
+    // function numeroAleatorio() {
+    //     return Math.random()
+    //   }
+    //   console.log(numeroAleatorio());
+    // arrow function way
+    //   const numeroAleatorio2 = () => Math.random();
+    //   console.log(numeroAleatorio2())
+
+    // function hello(nome) {
+    //     return `Olá, ${nome}!`
+    //   }
+    //   let nome = 'Ivan';
+    //   console.log(hello(nome));
+
+    //   const olar = nome => `Ola, ${nome}!`;
+    //   console.log(olar(nome))
+
+    // let nome = 'Ivan';
+    // let sobrenome = 'Pires';
+
+    // function nomeCompleto(nome, sobrenome) {
+    //     return `${nome} ${sobrenome}`
+    //   }
+    //   const nomeCompleto2 = (nome, sobrenome) =>`${nome} ${sobrenome}`
+
+
+    //   console.log(nomeCompleto(nome, sobrenome))
+    //   console.log(nomeCompleto2(nome, sobrenome));
+    let speed = 90;
+    const speedCar = (speed) => {
+      if (speed >= 120) {
+        return `Você ultrapassou o limite de velocidade`;
+      } else {
+        return `Você está na velocidade permitida`;
+      }
+    };
+    console.log(speedCar(speed));
+
+    let speed2 = 190;
+    const speedCar2 = (speed2) => (speed2 >= 120 ? 
+        `Você ultrapassou o limite de velocidade`:
+        `Você está na velocidade permitida`);
+    console.log(speedCar(speed2));
