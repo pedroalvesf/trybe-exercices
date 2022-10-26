01504d42f1361127e9cd81cc3af6efe946f9ed5e
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-26 17:09:34 -0300

    avancando na aula dia 5

--- fundamentos/bloco-4-javascript/dia5-js-let-cons-arrow.js
+++ fundamentos/bloco-4-javascript/dia5-js-let-cons-arrow.js
@@ -12,10 +12,30 @@
 
 // console.log(`Ola ${nome}`)
 
-let pessoa = {
-    nome: 'Pedro',
-    idade: 28,
-    anoNascimento: 1994
-}
+// let pessoa = {
+//     nome: 'Pedro',
+//     idade: 28,
+//     anoNascimento: 1994
+// }
 
-console.log(`Nome: ${pessoa.nome} \nTem ${pessoa.idade} anos de idade \nNasceu em ${pessoa.anoNascimento}`)
+// console.log(`Nome: ${pessoa.nome} 
+// \nTem ${pessoa.idade} anos de idade 
+// \nNasceu em ${pessoa.anoNascimento}`)
+
+// const multiplyByTwo = number => number * 2;
+// console.log(multiplyByTwo(10));
+
+// const multiplication = (number, multi) => number * multi;
+// console.log(multiplication(8, 2));
+
+let x = 3;
+function func(bool){
+    if(bool){
+        x =48;
+    }
+    return x;
+}
+console.log(x)
+console.log(func(true))
+console.log(func(false))
+console.log(x)
