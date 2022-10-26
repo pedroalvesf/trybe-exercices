7832b260144cb7374917040071807a33fd9bcbd1
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-26 16:10:13 -0300

    avancando no estudo do dia 5

---
+++ fundamentos/bloco-4-javascript/dia5-js-let-cons-arrow.js
@@ -0,0 +1,21 @@
+// // Com o template literals
+// console.log(`Primeira linha;
+// Segunda linha;
+// Terceira linha;`
+// );
+
+// // Sem o template literals:
+// console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
+
+// let nome = 'Pedro';
+
+
+// console.log(`Ola ${nome}`)
+
+let pessoa = {
+    nome: 'Pedro',
+    idade: 28,
+    anoNascimento: 1994
+}
+
+console.log(`Nome: ${pessoa.nome} \nTem ${pessoa.idade} anos de idade \nNasceu em ${pessoa.anoNascimento}`)
