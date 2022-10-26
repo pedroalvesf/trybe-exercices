10fa3e2c7cdf10baa86db48b3186068c7e75105b
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-26 13:30:39 -0300

    questao 2 exercicio academia de logica

--- fundamentos/bloco-4-javascript/dia5-academia-de-logica.js
+++ fundamentos/bloco-4-javascript/dia5-academia-de-logica.js
@@ -9,16 +9,45 @@
   
   // Fórmulas: perimetro = (2 * base) + (2 * altura) area = base * altura
 
-  function calculaAreaEPerimetro(base, altura){
-    let objeto = {
-        area: 0,
-        perimetro: 0
-    }
+//   function calculaAreaEPerimetro(base, altura){
+//     let objeto = {
+//         area: 0,
+//         perimetro: 0
+//     }
     
-     objeto['area'] = base * altura;
-     objeto.perimetro = (2*base) + (2*altura)
+//      objeto['area'] = base * altura;
+//      objeto.perimetro = (2*base) + (2*altura)
 
-    return objeto;
-  }
+//     return objeto;
+//   }
 
-  console.log(calculaAreaEPerimetro(2,4))
+//   console.log(calculaAreaEPerimetro(2,4))
+
+// Exercício 2 - Crie uma função que, dado um array de números inteiros,
+// retorne a quantidade de números pares e ímpares no formato abaixo:
+
+// let numeros = {
+//     pares: 0,
+//     ímpares: 0,
+//   };
+
+let numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13];
+
+function contaParesEImpares(numbers){
+    let numeros = {
+        contaPares: 0,
+        contaImpares: 0
+    }
+   
+    for(let index in numbers){
+        if(numbers[index] % 2 == 0){
+            numeros.contaPares += 1;
+        }else{
+            numeros.contaImpares += 1;
+        }
+    }
+    return numeros;
+}
+
+console.log(contaParesEImpares(numbers))
+
