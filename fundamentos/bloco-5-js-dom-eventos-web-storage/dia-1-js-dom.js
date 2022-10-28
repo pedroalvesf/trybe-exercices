a8105d6ae5d93fbf563925f1d09fb5ff0fa1514e
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-28 15:01:13 -0300

    avancando no estudo do dia 1 do bloco 6

---
+++ fundamentos/bloco-5-js-dom-eventos-web-storage/dia-1-js-dom.js
@@ -0,0 +1,8 @@
+//document.getElementById("IdDoElemento") = 
+
+document.getElementById("paragraph");
+document.getElementById("paragraph").innerText = 'Star Wars'
+paragraph.style.color = "red";
+
+document.getElementById("page-title").innerText = 'Best movie';
+
