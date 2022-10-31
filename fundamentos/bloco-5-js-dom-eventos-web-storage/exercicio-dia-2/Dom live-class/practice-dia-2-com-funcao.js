516b3e5088875f9c7237a42883a756b31981f544
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-31 18:55:07 -0300

    trazendo informacoes da aula

---
+++ fundamentos/bloco-5-js-dom-eventos-web-storage/exercicio-dia-2/Dom live-class/practice-dia-2-com-funcao.js
@@ -0,0 +1,36 @@
+// Inserindo novas figuras geométricas na página
+// PASSO 1: Capturar o elemento que será pai.
+
+const pai = document.querySelector('#first');
+
+// const filho = document.createElement('div');
+// filho.className = 'yellow square small';
+
+// pai.appendChild(filho);
+
+
+// Criar função que cria elemento filho
+
+const criaFilho = (classes) => {
+  const filho = document.createElement('div');
+  filho.className = classes;
+  return filho;
+}
+
+// Criar outra função que relaciona pai e filho
+
+const paiFilho = (filho) => {
+  pai.appendChild(filho);
+  // return pai;
+}
+
+// paiFilho(criaFilho('yellow circle small'));
+// paiFilho(criaFilho('red circle small'));
+// paiFilho(criaFilho('green circle small'));
+
+let classes = ['yellow circle small', 'green square small', 'red circle small'];
+
+for (let index of classes) {
+  let element = criaFilho(index); // faz a função do filho
+  paiFilho(element);
+}
