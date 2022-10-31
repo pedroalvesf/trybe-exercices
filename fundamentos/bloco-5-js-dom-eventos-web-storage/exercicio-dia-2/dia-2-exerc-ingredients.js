bedfcf9f38bf3f240c93493347bd7e5b75fcbac5
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-10-31 16:22:45 -0300

    iniciando aula aovivo

--- fundamentos/bloco-5-js-dom-eventos-web-storage/exercicio-dia-2/dia-2-exerc-ingredients.js
+++ fundamentos/bloco-5-js-dom-eventos-web-storage/exercicio-dia-2/dia-2-exerc-ingredients.js
@@ -14,6 +14,18 @@ let ingredient = ingredientItems[index];
 
 let ingredientListItem = document.createElement('li');
 ingredientListItem.innerText = ingredient;
+ingredientListItem.className = 'ingredients-list-item'
 
 ingredientList.appendChild(ingredientListItem);
+}
+
+let ingredientListItems = document.querySelectorAll('.ingredients-list-item');
+console.log(ingredientListItems);
+
+for(let index = 0; index < ingredientsListItems.length; index++){
+    let element = ingredientsListItems[index];
+
+    if(element.innerText.includes('200g')){
+        ingredientList.removeChild(element);
+    }
 }
