b99c66905bf2ead70d9240613dd29bd42bfceef9
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-11-01 11:23:58 -0300

    avnacando

--- fundamentos/bloco-exercicios-diversos/skins-desire-csgo/style.js
+++ fundamentos/bloco-exercicios-diversos/skins-desire-csgo/style.js
@@ -1,30 +1,30 @@
-const skinTextContainer = document.querySelector('.skin-text');
+const skinTextInput = document.querySelector('.skin-text-input');
 const addSkinButton = document.querySelector('.new-skin-button');
 
-const validateInput = () => {
-     if(skinTextContainer.length.trim() > 0 ){
-         return true
-     }else{
-        return alert("Invalid input")
-     }
- }
+const validateInput = () => skinTextInput.value.trim().length > 0;
+const validateInputNeutral = () => skinTextInput.value.trim().length == 0;
+
 
 const handleAddSkin = () => {
     const inputIsValid = validateInput();
 
     if(!inputIsValid){
-        return inputElement.classList.add("error");
+        return skinTextInput.classList.add("error");
     }
+
+    const skinItemContainer = document.createElement('div')
+    skinItemContainer.classList.add('skin-item')
 }
 
 const handleInputChange = () => {
-    const inputIsValid = validadeInput();
+    const inputIsValid = validateInput();
+    const inputIsNeutral = validateInputNeutral();
 
-    if(inputIsValid) {
-        return inputElement.classList.remove("error")
+    if(inputIsValid || inputIsNeutral) {
+        return skinTextInput.classList.remove("error")
     }
-}
+};
 
 addSkinButton.addEventListener("click", () => handleAddSkin());
 
-inputElement.addEventListener("change", () => handleInputChange());
+skinTextInput.addEventListener("change", () => handleInputChange());
