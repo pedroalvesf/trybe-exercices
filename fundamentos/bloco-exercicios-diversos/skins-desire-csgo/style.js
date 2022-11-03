d7d29d115f795d3f9ef147e5a5bd071513008685
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-11-03 17:22:00 -0300

    ajustando botao de apagar

--- fundamentos/bloco-exercicios-diversos/skins-desire-csgo/style.js
+++ fundamentos/bloco-exercicios-diversos/skins-desire-csgo/style.js
@@ -8,6 +8,7 @@ let skinLisTest = [
 const skinTextInput = document.querySelector('.skin-text-input'); //texto
 const addSkinButton = document.querySelector('.new-skin-button'); // button
 const containerSkin = document.querySelector('.skin-list-container');
+const containerMain = document.querySelector('.container-main');
 
 const validateInput = () => skinTextInput.value.trim().length > 0;
 const validateInputNeutral = () => skinTextInput.value.trim().length == 0;
@@ -32,25 +33,6 @@ const handleAddSkin = () => {
 
     }
 }
-function createRemoveButton(newSkin){
-    const newSkinRemove = document.createElement('button')
-    newSkinRemove.className = 'skin-item-remove-button';
-    newSkinRemove.innerHTML = 'Remove';
-    newSkin.appendChild(newSkinRemove);
-}
-
-
-const removeSkin = () => {
-    
-    const removeButton = document.querySelectorAll('.skin-item-remove-button');
-    for(let index = 0; index < removeButton.length; index++){
-    removeButton[index].addEventListener('click', (event) => {
-        event.target.parentNode.remove()
-    })
-}
-}
-
-
 
 const handleInputChange = () => {
     const inputIsValid = validateInput();
@@ -61,7 +43,45 @@ const handleInputChange = () => {
     }
 };
 
+function createRemoveButton(newSkin) {
+    const newSkinRemove = document.createElement('button');
+    newSkinRemove.className = 'skin-item-remove-button';
+    newSkinRemove.innerHTML = 'Remove';
+    newSkin.appendChild(newSkinRemove);
+}
 
+const removeSkin = () => {
+    const removeButton = document.querySelectorAll('.skin-item-remove-button');
+    for (let index = 0; index < removeButton.length; index++) {
+        removeButton[index].addEventListener('click', (event) => {
+            event.target.parentNode.remove();
+        })
+    }
+}
+
+function createRemoveAll() {
+    const removeAllButton = document.createElement('button');
+    removeAllButton.className = 'remove-all-button';
+    removeAllButton.innerHTML = 'Clean All';
+    containerMain.appendChild(removeAllButton);
+
+
+}
+createRemoveAll();
+
+function removeAll() {
+    const removeAllButton = document.querySelector('.remove-all-button')
+    const containerSkin = document.querySelector('.skin-list-container');
+        removeAllButton.addEventListener('click', () => { 
+        containerSkin.innerHTML = ''
+
+    })
+}
+removeAll();
+
+// event.target.document.getElementsByClassName('skin-list-container').reset();
+// })
+// document.getElementsByClassName('skin-list-container').reset();
 addSkinButton.addEventListener("click", handleAddSkin);
 //addSkinButton.addEventListener("click", () => handleAddSkin());
 skinTextInput.addEventListener("change", () => handleInputChange());
