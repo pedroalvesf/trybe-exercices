4889fd31498fbaebca88653a54b90a834ad3057f
PedroAlvesf <pedro.alves.hf@gmail.com>
2022-11-04 14:16:33 -0300

    salvando

--- fundamentos/bloco-5-js-dom-eventos-web-storage/exerc-pixel/script.js
+++ fundamentos/bloco-5-js-dom-eventos-web-storage/exerc-pixel/script.js
@@ -4,32 +4,42 @@ const pixelCelContainer = document.querySelector(".container-pixel");
 //}
 const btnChangeSize = document.querySelector(".btn-size")
 const btnChangeColors = document.querySelector(".btn-new-colors")
+const btnErase = document.querySelector(".btn-erase")
 const pixelSize = document.querySelector(".size")
 
-function createPixel(){
-    btnChangeSize.addEventListener('click', () =>{
-    for(let index = 0; index < pixelSize.value; index++){
-        let pixelCel = document.createElement('div');
-        pixelCel.setAttribute("class", "pixelCel");
-        pixelCelContainer.appendChild(pixelCel);
-    }
-})
+function createPixel() {
+    btnChangeSize.addEventListener('click', () => {
+        for (let index = 0; index < pixelSize.value; index++) {
+            let pixelCel = document.createElement('div');
+            pixelCel.setAttribute("class", "pixelCel");
+            pixelCelContainer.appendChild(pixelCel);
+        }
+    })
 }
 createPixel();
 
-function changeColors(){
-    btnChangeColors.addEventListener('click', (event) =>{
+function changeColors() {
+    btnChangeColors.addEventListener('click', (event) => {
         event.target.newColors()
     })
 }
-
+//confirmar se vai ser usando rgb ou hexadecimal para as cores 
 function newColors() {
     let r = Math.floor(Math.random() * 255);
     let g = Math.random() * 255;
     let b = Math.random() * 255;
     return `rgb(${r}, ${g}, ${b})`;
- }
- newColors();
+}
+newColors();
+
+function eraseColor(pixelCel) {
+    btnErase.addEventListener('click', () => {
+        for (let index = 0; index < pixelCel.length; index++) {
+            pixelCel[index].style.backgroundColor('white')
+        }
+    })
+}
+eraseColor();
 
  //ul.addEventListener('mouseover', (event) =>{
  // event.target.classlist.add('mouseOver')
