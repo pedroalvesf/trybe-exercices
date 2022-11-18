19a16ffc49d7b71c2faaec77200e60682a2a2375
Pedro Alves <pedro.alves.hf@gmail.com>
2022-11-18 18:01:31 -0300

    pause

--- fundamentos/bloco-7/dia1-script.js
+++ fundamentos/bloco-7/dia1-script.js
@@ -1,27 +1,51 @@
 const pessoaEstudante = {
-    firstName:'Pedro',
-}
+  firstName: "Pedro",
+};
 
-let newKeyObj = 'lastName';
-const lastName = 'Alves';
+let newKeyObj = "lastName";
+const lastName = "Alves";
 
-
-
-const addProprierty = (pessoaEstudante, newKeyObj, lastName) =>{
-    pessoaEstudante[newKeyObj] = lastName;
-
+const addProprierty = (pessoaEstudante, newKeyObj, lastName) => {
+  pessoaEstudante[newKeyObj] = lastName;
 };
 
 addProprierty(pessoaEstudante, newKeyObj, lastName);
+console.log(pessoaEstudante);
 
-addProprierty(pessoaEstudante, 'age', '28');
+addProprierty(pessoaEstudante, "age", "28");
+console.log(pessoaEstudante);
 
-newkey = 'fullName';
+newkey = "fullName";
 const fullName = `${pessoaEstudante.firstName} ${pessoaEstudante.lastName}`;
 
+addProprierty(pessoaEstudante, newkey, fullName);
+console.log(pessoaEstudante);
 
-addProprierty(pessoaEstudante, newKeyObj, fullName);
+console.log("----------------------------------------------");
 
 
-console.log(pessoaEstudante)
+const student = {
+  html: "Muito Bom",
+  css: "Bom",
+  javascript: "Ótimo",
+  softskill: "Ótimo",
+};
 
+const listSkillsValuesWithFor = (student) => {
+  const skills = [];
+  for (skill in student) {
+    skills.push(student[skill]);
+  }
+
+  return skills;
+};
+
+const listSkillsValuesWithValues = (student) => {
+  return Object.values(student);
+}; 
+
+// Sem Object.values
+console.log(listSkillsValuesWithFor(student));
+
+// Com Object.values
+console.log(listSkillsValuesWithValues(student));
