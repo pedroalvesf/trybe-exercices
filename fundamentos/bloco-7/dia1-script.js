9c8a27d05e475345c5fc78a968bd200818f5bf48
Pedro Alves <pedro.alves.hf@gmail.com>
2022-11-20 12:31:38 -0300

    starting

--- fundamentos/bloco-7/dia1-script.js
+++ fundamentos/bloco-7/dia1-script.js
@@ -72,3 +72,11 @@ function sum() {
     const button = document.getElementById('button');
     button.addEventListener('click', sum);
   }
+
+// Lançar erros com throw e capturá-los com o bloco try/catch;
+
+// Acessar todas as chaves de um objeto com o método Object.keys;
+
+// Acessar os pares chave-valor de um objeto com o método Object.entries;
+
+// Acessar todos os valores de um objeto com o método Object.values;
