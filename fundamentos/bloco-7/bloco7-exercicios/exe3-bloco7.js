3997ab84889c0c11f0320c0a33ba05aa4cba59dd
Pedro Alves <pedro.alves.hf@gmail.com>
2022-11-20 20:40:25 -0300

    avancando

--- fundamentos/bloco-7/bloco7-exercicios/exe3-bloco7.js
+++ fundamentos/bloco-7/bloco7-exercicios/exe3-bloco7.js
@@ -1,7 +1,3 @@
-// Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
-
-// Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
-
 const lesson1 = {
     materia: 'Matemática',
     numeroEstudantes: 20,
@@ -24,40 +20,97 @@ const lesson3 = {
 // lesson2.turno = 'noite';
 console.log(lesson2);
 
-const addTurno2 = (obj, newKey, value) =>{
+const addNewKey = (obj, newKey, value) =>{
 obj[newKey] = value;
 }
 
-addTurno2(lesson2, 'turno', 'noite');
+addNewKey(lesson2, 'turno', 'noite');
 console.log(lesson2);
 console.log("XXXXXXXXXXXXXXXX");
 
 
 // Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 
-const showKeys = (obj) =>{
-    const keysOfObj = Object.keys(obj)
-    for (const index in keysOfObj){
-        console.log(keysOfObj[index])
-    }
-}
-showKeys(lesson1);
-console.log("XXXXXXXXXXXXXXXX");
-function showKeys2(obj){
-    const keysOfObj = Object.keys(obj)
-}
-
-console.log(showKeys2(lesson1)); //Why undefined?!
-// console.log(Object.keys(lesson1));
+// const showKeys = (obj) =>{
+//     const keysOfObj = Object.keys(obj)
+//     for (const index in keysOfObj){
+//         console.log(keysOfObj[index])
+//     }
+// }
+//showKeys(lesson1);
+console.log("111111111111111111111111");
+const showKeys =(obj) =>Object.keys(obj);
+console.log(showKeys(lesson1))
+console.log("111111111111111111111111");
 console.log("XXXXXXXXXXXXXXXX");
 
 // Crie uma função para mostrar o tamanho de um objeto.
 
-const showKeysCounte = (obj) =>{    
-    const keysOfObj = Object.keys(obj).length
+const showKeysCounte = (obj) => Object.keys(obj).length
+
+console.log(showKeysCounte(lesson1));
+console.log("LLLLLLLLLLLLLLLL");
+
+// Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const keysValues = (obj) => Object.values(obj);
+console.log(keysValues(lesson2));
+
+console.log("PPPPPPPPPPPPPPPPPP");
+
+// Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
+
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+console.log(allLessons);
+
+
+console.log("OOOOOOOOOOOOOOOOOOOO");
+// Usando o objeto criado no tópico anterior, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+const sumEstudants = allLessons.lesson1.numeroEstudantes + allLessons.lesson2.numeroEstudantes+ allLessons.lesson3.numeroEstudantes;
+console.log(sumEstudants);
+
+console.log(allLessons.lesson1.numeroEstudantes + allLessons.lesson2.numeroEstudantes+ allLessons.lesson3.numeroEstudantes);
+
+const getAllStudents = (obj) =>{
+    let total = 0;
+
+    const keys = Object.keys(obj);
+
+    for (index in keys){
+        total +=obj[keys[index]].numeroEstudantes;
+    }
+    return total;
 }
 
-function keysCount(obj){
-    const keysOfObj = Object.keys(obj).length
+console.log(getAllStudents(allLessons))
+
+console.log("OOOOOOOOOOOOOOOOOOOO");
+
+// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+
+const valuePosition = (obj, number) => Object.values(obj)[number];
+const keyPosition = (obj, number) => Object.keys(obj)[number];
+console.log(keyPosition(lesson2, 2) + ' ' + valuePosition(lesson2, 2));
+
+console.log("8888888888888888888888");
+// Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles o objeto, o nome da chave e o valor da chave. Exemplo:
+
+const verifyPair = (obj, keys, value ) =>{
+    const entries = Object.entries(obj);
+    let isTrue = false;
+    for(let index in entries){
+        if(entries[index][0] === keys && entries[index][1] == value)
+        isTrue = true;
+    }
+    return isTrue
 }
-console.log(keysCount(lesson1))// WWHY?!
+
+console.log(verifyPair(lesson2, 'professor', 'Carlos'));
+console.log("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK")
+
+// Utilizando o objeto (allLesson), crie uma função para contar quantos estudantes assistiram às aulas de Matemática;
+
+console.log("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK")
+
+// Utilizando o objeto (allLesson), crie uma função que deva retornar um objeto que represente o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes.
