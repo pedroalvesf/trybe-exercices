216ad55d3a2a1e8a0865c20d4193080b25404319
Pedro Alves <pedro.alves.hf@gmail.com>
2022-11-21 18:22:53 -0300

    testing

---
+++ fundamentos/bloco-7/jestStudy/myremove2/myRemove.test.js
@@ -0,0 +1,9 @@
+const myRemove = require('./myRemove.js');
+
+describe('Testa a funcao de remover item', () => {
+    it('deve retonar um array menor', () => {
+        expect(myRemove([1,2,3,4], 3)).toEqual([1,2,4]);
+        expect(myRemove([1,2,3,4], 2)).toEqual([1,2,4]);
+        expect(myRemove([1,2,3,4], 2)).not.toEqual([1,2,3,4]);
+    });
+});
