1765a2ff0af0014d043ccfab6d441ef02b82381a
Pedro Alves <pedro.alves.hf@gmail.com>
2022-11-21 16:49:57 -0300

    atualizando

--- fundamentos/bloco-7/myNewProject/exercicios-dia-2/myRemove.test.js
+++
@@ -1,9 +0,0 @@
-const myRemove = require('./myRemove');
-
-describe('testa a função myRemove', () => {
-it('deve retornar um array removendo o item', () => {
-    expected(myRemove([1,2,3,4], 3)).toEqual([1,2,4]);
-    expected(myRemove([1,2,3,4], 3)).not.toEqual([1,2,3,4]);
-    expected(myRemove([1,2,3,4], 5)).toEqual([1,2,3,4]);
-});
-});
