84d847b8bed6cdead85ca56c75fb7de8fea0e4d7
Pedro Alves <pedro.alves.hf@gmail.com>
2022-11-21 17:11:02 -0300

    second question ok

--- fundamentos/bloco-7/jestStudy/myFizzBuzz/myFizzBuzz.test.js
+++ fundamentos/bloco-7/jestStudy/myFizzBuzz/myFizzBuzz.test.js
@@ -1,1 +1,11 @@
-const myFizzBuzz = requite('./myFizzBuzz.js');
+const myFizzBuzz = require('./myFizzBuzz.js');
+
+describe('testa a funcao myFizzBuzz', () => {
+    it('veririca, se de acordo com o parametro, retorna o valor esperado', () =>{
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
+    expect(myFizzBuzz(3)).toBe('fizz');
+    expect(myFizzBuzz(5)).toBe('buzz');
+    expect(myFizzBuzz(1)).toBe(1);
+    expect(myFizzBuzz('2')).toBe(false);
+    })
+})
