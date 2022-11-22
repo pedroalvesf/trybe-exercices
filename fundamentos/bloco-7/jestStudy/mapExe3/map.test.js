5cd7d1438b7fa71665c9581428eb09fd36c7020c
Pedro Alves <pedro.alves.hf@gmail.com>
2022-11-21 21:43:48 -0300

    .

--- fundamentos/bloco-7/jestStudy/mapExe3/map.test.js
+++ fundamentos/bloco-7/jestStudy/mapExe3/map.test.js
@@ -1,14 +1,51 @@
-const decode = require('./decode.js');
-// const encode = require('./encode.js');
-
-describe('Testando se sao funcoes', () => {
+const { encode, decode } = require('./encodeDecode.js');
+describe('Testando as duas funcoes', () => {
     it('deve confirmar se `e uma funcao', () => {
+        expect(decode).toBeDefined();
+    });
+    it('decode `e uma funcao', () => {
         expect(typeof decode).toEqual('function');
     });
+    it('conver apenas a vogal "a" em numero 1', () => {
+        expect(encode('ana')).toEqual('1n1');
+    });
+    it('converte a vogal "e" no número 2', () => {
+        expect(encode('ele')).toEqual('2l2');
+    });
+    it('converte a vogal "i" no número 3', () => {
+        expect(encode('xixi')).toEqual('x3x3');
+    });
+    it('converte a vogal "o" no número 4', () => {
+        expect(encode('ovo')).toEqual('4v4');
+    });
+    it('converte a vogal "u" no número 5', () => {
+        expect(encode('nu')).toEqual('n5');
+    });
+    it('checa se o retorno da função tem o mesmo número de caracteres', () => {
+        expect(encode('trybe').length).toEqual(5);;
+    });
+    it('a função decode é definida', () => {
+        expect(decode).toBeDefined();
+    });
+    it('decode é uma função', () => {
+        expect(typeof decode).toEqual('function');
+    });
+    it('converte apenas o número 1 na vogal a', () => {
+        expect(decode('1n1')).toEqual('ana');
+    });
+    it('converte apenas o número 2 na vogal e', () => {
+        expect(decode('2l2')).toEqual('ele');
+    });
+    it('converte apenas o número 3 na vogal i', () => {
+        expect(decode('x3x3')).toEqual('xixi');
+    });
+    it('converte apenas o número 4 na vogal o', () => {
+        expect(decode('4v4')).toEqual('ovo');
+    });
+    it('converte apenas o número 5 na vogal u', () => {
+        expect(decode('n5')).toEqual('nu');
+    });
+    it('checa se o retorno da função tem o mesmo número de caracteres', () => {
+        expect(decode('trybe').length).toEqual(5);
+    });
 });
-
-// describe('testando a troca das letras', () => {
-//     it('confirmando trocas de letras por numeros', () => {
-//         expect(encode('a', 'e', 'i', 'o', 'u')).toEqual('1', '2', '3', '4', '5');
-//     })
-// })
