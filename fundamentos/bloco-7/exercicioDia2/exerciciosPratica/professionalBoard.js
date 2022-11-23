e63f1ae17e538b2ed99b3daaabdd6895d183b8b6
Pedro Alves <pedro.alves.hf@gmail.com>
2022-11-22 22:22:17 -0300

    falta detalhe do ultimo

---
+++ fundamentos/bloco-7/exercicioDia2/exerciciosPratica/professionalBoard.js
@@ -0,0 +1,55 @@
+// Dados
+const professionalBoard = [
+    {
+        id: '8579-6',
+        firstName: 'Ana',
+        lastName: 'Gates',
+        specialities: ['UX', 'Design'],
+    },
+    {
+        id: '5569-4',
+        firstName: 'George',
+        lastName: 'Jobs',
+        specialities: ['Frontend', 'Redux', 'React', 'CSS'],
+    },
+    {
+        id: '4456-4',
+        firstName: 'Leila',
+        lastName: 'Zuckerberg',
+        specialities: ['Context API', 'RTL', 'Bootstrap'],
+    },
+    {
+        id: '1256-4',
+        firstName: 'Linda',
+        lastName: 'Bezos',
+        specialities: ['Hooks', 'Context API', 'Tailwind CSS'],
+    },
+    {
+        id: '9852-2-2',
+        firstName: 'Jeff',
+        lastName: 'Cook',
+        specialities: ['Ruby', 'SQL'],
+    },
+    {
+        id: '4678-2',
+        firstName: 'Paul',
+        lastName: 'Dodds',
+        specialities: ['Backend'],
+    },
+];
+
+// Pesquisa
+const searchEmployee = (id, detail) => {
+    // Implemente seu código aqui
+    // professionalBoard.forEach((profissional) => {
+    //     if(profissional.id === id){
+    //         return profissional[detail];
+    //     }
+    // })
+    for(let index of professionalBoard){
+        if(index.id === id){
+            return index[detail]
+        }
+    }
+};
+console.log(searchEmployee("4678-2", "specialities"));
