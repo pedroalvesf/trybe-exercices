91749fcb6dc4195ed193746c987bbb49fdb1e757
Pedro Alves <pedro.alves.hf@gmail.com>
2022-11-29 13:57:45 -0300

    studying

--- fundamentos/bloco-8/HOF/hof-ainicio.js
+++ fundamentos/bloco-8/HOF/hof-ainicio.js
@@ -15,7 +15,7 @@ console.log(firstObject);
 
 const idades = [18, 21, 42, 20, 19, 21, 30, 73, 82, 45, 48, 50];
 
-const idadeFind = idades.find((idade) => idade > 80);
+const idadeFind = idades.every((idade) => idade >= 18);
 console.log(idadeFind);
 
 const musicas = [
@@ -29,15 +29,13 @@ const musicas = [
 const findSong = musicas.find(song => song.id === '31031685');
 console.log(findSong);
 
-
-
 const grades = {
   portugues: 'Aprovado',
   matematica: 'Reprovado',
   ingles: 'Aprovado',
 };
 
-const verifyGrades = Object.values(grades).some((grade) => grade.toLowerCase() === 'aprovado'); // false
+const verifyGrades = Object.values(grades).some((grade) => grade.toLowerCase() === 'aprovado');
 
 console.log(verifyGrades);
 console.log("-----------------------");
@@ -67,4 +65,46 @@ const verifyAges = (arr, number) => {
 };
 console.log(verifyAges(people, 18));
 console.log(verifyAges(people, 14));
+console.log("-----------------------");
+console.log("-----------------------");
 
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+// Adicione seu código aqui
+
+const mandaLa = emailListInData.map((element) => {
+  return `Email cadastrado: ${element}`;
+})
+console.log(mandaLa);
+
+console.log(emailListInData.map((element) => {
+  return `Email cadastrado: ${element}`;
+}));
+
+console.log("-----------------------");
+
+const mandaLa2 = (element) => {
+  console.log(`Email cadastrado: ${element}`);
+};
+emailListInData.forEach(mandaLa2);
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'),  // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+const geraEmail = (name) => {
+  const email = name.toLowerCase().replace(' ', '_' );
+  return{name, email:`${email}@trybe.com`}
+}
+
+console.log(newEmployees(geraEmail));
--- fundamentos/bloco-8/HOF/hof-loteria.js
+++ fundamentos/bloco-8/HOF/hof-loteria.js
@@ -1,27 +1,9 @@
-const RIGHT_ANSWERS =   ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
-const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+const check = (num, numero) => num === numero;
 
-function nota() {
-    let resultado  = 0;
-    let cC = 0;
-    let CW = 0;
-    let Cn = 0;
-    for (let index in RIGHT_ANSWERS){
-        if (RIGHT_ANSWERS[index] ===  STUDENT_ANSWERS[index]){
-            resultado += 1;
-            cC += 1;
-        }else if (STUDENT_ANSWERS[index] ===  'N.A'){
-            resultado += 0;
-            CW += 1;
-        }else if (RIGHT_ANSWERS[index] !==  STUDENT_ANSWERS[index]){
-            resultado -= 0.5;
-            Cn += 1;
-        }
-    }
-    console.log("CC" + cC);
-    console.log("CW" + CW);
-    console.log("Cn" + Cn);
-    return resultado;
-}
-console.log(nota());
+const result = (num, callback) => {
+    const numero = Math.floor((Math.random() * 5) + 1);
+    return callback(num, numero) ? "boa" : "bad";
+};
+
+console.log(result(2, check));
 
