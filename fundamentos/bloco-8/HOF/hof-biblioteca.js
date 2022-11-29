b23a96c3e2d56964129edeef908633251f4f852f
Pedro Alves <pedro.alves.hf@gmail.com>
2022-11-29 19:36:25 -0300

    rpg

--- fundamentos/bloco-8/HOF/hof-biblioteca.js
+++ fundamentos/bloco-8/HOF/hof-biblioteca.js
@@ -1,104 +1,232 @@
+// const books = [
+//     {
+//         id: 1,
+//         name: 'As Crônicas de Gelo e Fogo',
+//         genre: 'Fantasia',
+//         author: {
+//             name: 'George R. R. Martin',
+//             birthYear: 1948,
+//         },
+//         releaseYear: 1991,
+//     },
+//     {
+//         id: 2,
+//         name: 'O Senhor dos Anéis',
+//         genre: 'Fantasia',
+//         author: {
+//             name: 'J. R. R. Tolkien',
+//             birthYear: 1892,
+//         },
+//         releaseYear: 1954,
+//     },
+//     {
+//         id: 3,
+//         name: 'Fundação',
+//         genre: 'Ficção Científica',
+//         author: {
+//             name: 'Isaac Asimov',
+//             birthYear: 1920,
+//         },
+//         releaseYear: 1951,
+//     },
+//     {
+//         id: 4,
+//         name: 'Duna',
+//         genre: 'Ficção Científica',
+//         author: {
+//             name: 'Frank Herbert',
+//             birthYear: 1920,
+//         },
+//         releaseYear: 1965,
+//     },
+//     {
+//         id: 5,
+//         name: 'A Coisa',
+//         genre: 'Terror',
+//         author: {
+//             name: 'Stephen King',
+//             birthYear: 1947,
+//         },
+//         releaseYear: 1986,
+//     },
+//     {
+//         id: 6,
+//         name: 'O Chamado de Cthulhu',
+//         genre: 'Terror',
+//         author: {
+//             name: 'H. P. Lovecraft',
+//             birthYear: 1890,
+//         },
+//         releaseYear: 1928,
+//     },
+// ];
+
+// console.log(books[4].author.name);
+// // Adicione o código do exercício aqui:
+
+// // const expectedResult = 'Stephen King';
+// const authorBornIn1947 = () => {
+//     // escreva aqui o seu código
+//     return books.find(book => book.author.birthYear === 1947).author.name;
+// };
+// authorBornIn1947();
+
+// // const expectedResult = 'Duna';
+// const smallerName = () => {
+//     let nameBook = 'aaaaaaaaaaaaaaaaaaaaaaa';
+//     // escreva aqui o seu código
+//     books.forEach((book) => {
+//         if (!nameBook || book.name.length < nameBook.length) {
+//             nameBook = book.name;
+//         }
+//     })
+//     // Variável nameBook que receberá o valor do menor nome;
+//     return nameBook;
+// }
+// console.log(smallerName(books));
+
+// const getNamedBook = () => {
+//     // escreva seu código aqui
+//     return books.find((book) => book.name.length === 26);
+// }
+// console.log(getNamedBook(books));
+
+// function everyoneWasBornOnSecXX() {
+//     // escreva seu código aqui
+//     return books.every((birth) => birth.author.birthYear >= 1900 && birth.author.birthYear<= 2000);
+// }
+// console.log(everyoneWasBornOnSecXX(books));
+
+// const someBookWasReleaseOnThe80s = () => {
+//     // escreva seu código aqui
+//     return books.some((eights) => eights.releaseYear >= 1980 && eights.releaseYear <= 1990);
+// };
+// console.log(someBookWasReleaseOnThe80s(books));
+
 const books = [
     {
-        id: 1,
-        name: 'As Crônicas de Gelo e Fogo',
-        genre: 'Fantasia',
-        author: {
-            name: 'George R. R. Martin',
-            birthYear: 1948,
-        },
-        releaseYear: 1991,
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
     },
     {
-        id: 2,
-        name: 'O Senhor dos Anéis',
-        genre: 'Fantasia',
-        author: {
-            name: 'J. R. R. Tolkien',
-            birthYear: 1892,
-        },
-        releaseYear: 1954,
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
     },
     {
-        id: 3,
-        name: 'Fundação',
-        genre: 'Ficção Científica',
-        author: {
-            name: 'Isaac Asimov',
-            birthYear: 1920,
-        },
-        releaseYear: 1951,
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
     },
     {
-        id: 4,
-        name: 'Duna',
-        genre: 'Ficção Científica',
-        author: {
-            name: 'Frank Herbert',
-            birthYear: 1920,
-        },
-        releaseYear: 1965,
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
     },
     {
-        id: 5,
-        name: 'A Coisa',
-        genre: 'Terror',
-        author: {
-            name: 'Stephen King',
-            birthYear: 1947,
-        },
-        releaseYear: 1986,
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
     },
     {
-        id: 6,
-        name: 'O Chamado de Cthulhu',
-        genre: 'Terror',
-        author: {
-            name: 'H. P. Lovecraft',
-            birthYear: 1890,
-        },
-        releaseYear: 1928,
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+    releaseYear: 1928,
     },
 ];
 
-console.log(books[4].author.name);
 // Adicione o código do exercício aqui:
+// const authorBornIn1947 = () => {
+//     // escreva aqui o seu código
+//     return books.find(book => book.author.birthYear === 1947).author.name;
+// };
+// console.log(authorBornIn1947());
 
-// const expectedResult = 'Stephen King';
+// const smallerName = () => {
+//     let nameBook = 'aaaaaaaaaaa'
+//     // escreva aqui o seu código
+//     books.forEach(book => {
+//         if(!nameBook || book.name.length < nameBook.length){
+//             nameBook = book.name;
+//         }
+//     });
+//     // Variável nameBook que receberá o valor do menor nome;
+//     return nameBook;
+// };
+
+// console.log(smallerName());
+
+// const getNamedBook = () => {
+//     // escreva seu código aqui
+//     return books.find(book => book.name.length === 26);
+// };
+// console.log(getNamedBook());
+
+// function everyoneWasBornOnSecXX() {
+//   // escreva seu código aqui
+//     return books.every(book => book.author.birthYear >= 1900 && book.author.birthYear <= 1999);
+
+// };
+// console.log(everyoneWasBornOnSecXX());
+
+// function testeReleased () {
+//     const released =  books.find(book => book.releaseYear === 1991);
+//     return released;
+// };
+// console.log(testeReleased());
+
+// const someBookWasReleaseOnThe80s = () => {
+//     // escreva seu código aqui
+//     return books.every(book => book.releaseYear >= 1880 && book.releaseYear < 3000);
+// };
+// console.log(someBookWasReleaseOnThe80s());
+
+
 const authorBornIn1947 = () => {
-    // escreva aqui o seu código
-    return books.find(book => book.author.birthYear === 1947).author.name;
+    return books.find(book => book.author.birthYear === 1947)
 };
-authorBornIn1947();
+console.log(authorBornIn1947());
 
-// const expectedResult = 'Duna';
 const smallerName = () => {
-    let nameBook = 'aaaaaaaaaaaaaaaaaaaaaaa';
-    // escreva aqui o seu código
-    books.forEach((book) => {
-        if (!nameBook || book.name.length < nameBook.length) {
+    let nameBook = 'aaaaaaaaa';
+    books.forEach(book => {
+        if(!nameBook || book.name.length < nameBook.length){
             nameBook = book.name;
-        }
+    }
+
     })
-    // Variável nameBook que receberá o valor do menor nome;
     return nameBook;
-}
-console.log(smallerName(books));
 
-const getNamedBook = () => {
-    // escreva seu código aqui
-    return books.find((book) => book.name.length === 26);
-}
-console.log(getNamedBook(books));
-
-function everyoneWasBornOnSecXX() {
-    // escreva seu código aqui
-    return books.every((birth) => birth.author.birthYear >= 1900 && birth.author.birthYear<= 2000);
-}
-console.log(everyoneWasBornOnSecXX(books));
-
-const someBookWasReleaseOnThe80s = () => {
-    // escreva seu código aqui
-    return books.some((eights) => eights.releaseYear >= 1980 && eights.releaseYear <= 1990);
 };
-console.log(someBookWasReleaseOnThe80s(books))
+console.log(smallerName());
--- fundamentos/bloco-8/HOF/hof-game-actions.js
+++ fundamentos/bloco-8/HOF/hof-game-actions.js
@@ -1,3 +1,157 @@
+// const mage = {
+//     healthPoints: 130,
+//     intelligence: 45,
+//     mana: 125,
+//     damage: undefined,
+// };
+
+// const warrior = {
+//     healthPoints: 200,
+//     strength: 30,
+//     weaponDmg: 2,
+//     damage: undefined,
+// };
+
+// const dragon = {
+//     healthPoints: 350,
+//     strength: 50,
+//     damage: undefined,
+// };
+
+// const battleMembers = { mage, warrior, dragon };
+
+// const dragonDamage = () => {
+//     const dragonAttack = Math.floor(Math.random() * (dragon.strength - 15) + 15);
+//     return dragonAttack;
+// };
+
+// // console.log("Dragon damage: " + dragonDamage() + "\nHealth: " + dragon.healthPoints);
+
+// const warriorDamage = () => {
+//     const warriorAttack = Math.floor(Math.random() * (warrior.strength*warrior.weaponDmg - warrior.strength) + warrior.strength);
+//     return warriorAttack;
+// };
+
+// // console.log("Warrior damage: " + warriorDamage() + "\nHealth: " + warrior.healthPoints);
+
+// const mageDamage = () => {
+//     let mageDmg = Math.floor(Math.random() * (mage.intelligence*2 - mage.intelligence) + mage.intelligence);
+//     let mageAttack = {
+//         manaCost: 15,
+//         dmg: mageDmg,
+//     };
+//     if(mage.mana < 15) {
+//         mageAttack = {
+//         manaCost: 0,
+//         dmg: 'Não possui mana suficiente',
+//         };
+//         return mageAttack;
+//     };
+//     return mageAttack;
+// };
+
+// // console.log("Mage damage: " + mageDamage().dmg + "\nHealth: " + mage.healthPoints);
+
+// const gameActions = {
+//     // Crie as HOFs neste objeto.
+//     warriorTurn: () => {
+//         const warriorAt =  warriorDamage();
+//         dragon.healthPoints -= warriorAt;
+//         warrior.damage = warriorAt;
+//     },
+//     mageTurn: () => {
+//         const mageAt =  mageDamage();
+//         mage.mana -= mageAt.manaCost;
+//         dragon.healthPoints -= mageAt.dmg;
+//         mage.damage = mageAt;
+//     },
+//     dragonTurn: () => {
+//         const dragonAt =  dragonDamage();
+//         mage.healthPoints -= dragonAt;
+//         warrior.healthPoints -= dragonAt;
+//         dragon.damage = dragonAt;
+//     },
+//     turnResults: () => battleMembers,
+// };
+
+// gameActions.warriorTurn();
+// gameActions.mageTurn();
+// gameActions.dragonTurn();
+// console.log(gameActions.turnResults());
+
+// const mage = {
+//     healthPoints: 130,
+//     intelligence: 45,
+//     mana: 125,
+//     damage: undefined,
+// };
+
+// const warrior = {
+//     healthPoints: 200,
+//     strength: 30,
+//     weaponDmg: 2,
+//     damage: undefined,
+// };
+
+// const dragon = {
+//     healthPoints: 350,
+//     strength: 50,
+//     damage: undefined,
+// };
+
+// const battleMembers = { mage, warrior, dragon };
+
+// const dragonDamage = () => {
+//     const dragonAttack = Math.floor(Math.random() * (dragon.strength - 15) + 15);
+//     return dragonAttack;
+// };
+// const warriorDamage = () => {
+//     const warriorAttack = Math.floor(Math.random() * (warrior.strength*warrior.weaponDmg - warrior.strength) + warrior.strength);
+//     return warriorAttack;
+// };
+
+// const mageDamage = () => {
+//     let mageDmg = Math.floor(Math.random() * (mage.intelligence*2 - mage.intelligence) + mage.intelligence);
+//     let mageAttack = {
+//         manaCost: 15,
+//         dmg: mageDmg,
+//     };
+//     if(mage.mana < 15) {
+//         mageAttack = {
+//         manaCost: 0,
+//         dmg: 'Não possui mana suficiente',
+//         };
+//         return mageAttack;
+//     };
+//     return mageAttack;
+// };
+
+// const gameActions = {
+//     warriorTurn: () => {
+//         const warriorAt =  warriorDamage();
+//         dragon.healthPoints -= warriorAt;
+//         warrior.damage = warriorAt;
+//     },
+//     mageTurn: () => {
+//         const mageAt =  mageDamage();
+//         mage.mana -= mageAt.manaCost;
+//         dragon.healthPoints -= mageAt.dmg;
+//         mage.damage = mageAt;
+//     },
+//     dragonTurn: () => {
+//         const dragonAt =  dragonDamage();
+//         mage.healthPoints -= dragonAt;
+//         warrior.healthPoints -= dragonAt;
+//         dragon.damage = dragonAt;
+//     },
+//     turnResults: () => battleMembers,
+// };
+
+// gameActions.warriorTurn();
+// gameActions.mageTurn();
+// gameActions.dragonTurn();
+// console.log(gameActions.turnResults());
+
 const mage = {
     healthPoints: 130,
     intelligence: 45,
@@ -12,63 +166,74 @@ const warrior = {
     damage: undefined,
 };
 
+const rogue = {
+    healthPoints: 150,
+    agility: 40,
+    weaponDmg: 4,
+    damage: undefined,
+};
+
 const dragon = {
     healthPoints: 350,
     strength: 50,
     damage: undefined,
 };
 
-const battleMembers = { mage, warrior, dragon };
+const battleMembers = { mage, warrior, rogue, dragon };
 
 const dragonDamage = () => {
     const dragonAttack = Math.floor(Math.random() * (dragon.strength - 15) + 15);
     return dragonAttack;
 };
 
-// console.log("Dragon damage: " + dragonDamage() + "\nHealth: " + dragon.healthPoints);
-
 const warriorDamage = () => {
-    const warriorAttack = Math.floor(Math.random() * (warrior.strength*warrior.weaponDmg - warrior.strength) + warrior.strength);
+    const warriorAttack = Math.floor(Math.random() * (warrior.strength * warrior.weaponDmg - warrior.strength) + warrior.strength);
     return warriorAttack;
-}
-
-// console.log("Warrior damage: " + warriorDamage() + "\nHealth: " + warrior.healthPoints);
-
-const mageDamage = () => {
-    let mageDmg = Math.floor(Math.random() * (mage.intelligence*2 - mage.intelligence) + mage.intelligence);
-    let mageAttack = {
-        manaCost: 15,
-        dmg: mageDmg,
-    };
-    if(mage.mana < 15) {
-        mageAttack = {
-        manaCost: 0,
-        dmg: 'Não possui mana suficiente',
-        };
-        return mageAttack;
-    };
-    return mageAttack;
 };
 
-// console.log("Mage damage: " + mageDamage().dmg + "\nHealth: " + mage.healthPoints);
+const rogueDamage = () => {
+    const rogueAttack = Math.floor(Math.random() * (rogue.agility*rogue.weaponDmg - rogue.agility) +rogue.agility);
+    return rogueAttack;
+};
+
+const mageDamage = () => {
+    let mageAttack = Math.floor(Math.random() * (mage.intelligence*2 - mage.intelligence) + mage.intelligence);
+    let mageAtk = {
+        manaCost: 15,
+        dmg: mageAttack,
+    }
+    if(mage.mana < 15){
+        mageAtk = {
+            manaCost:0,
+            dmg: "deu ruim",
+        }
+        return mageAttack;
+    }
+    return mageAttack;
+}
 
 const gameActions = {
-    // Crie as HOFs neste objeto.
     warriorTurn: () => {
-        const warriorAt =  warriorDamage();
+        const warriorAt = warriorDamage();
         dragon.healthPoints -= warriorAt;
-        warrior.damage = warriorAt;
+        warrior.damage = warriorAt
     },
     mageTurn: () => {
-        const mageAt =  mageDamage();
-        mage.mana -= mageAt.manaCost;
-        dragon.healthPoints -= mageAt.dmg;
+        const mageAt = mageDamage();
+        mage.mana -= 15;
+        dragon.healthPoints -= mageAt;
         mage.damage = mageAt;
     },
+    rogueTurn: () => {
+        const rogueAt = rogueDamage();
+        dragon.healthPoints -= rogueAt;
+        rogue.damage = rogueAt;
+    },
+
     dragonTurn: () => {
-        const dragonAt =  dragonDamage();
-        mage.healthPoints -= dragonAt;
+        const dragonAt = dragonDamage();
         warrior.healthPoints -= dragonAt;
+        mage.healthPoints -= dragonAt;
         dragon.damage = dragonAt;
     },
     turnResults: () => battleMembers,
@@ -77,4 +242,5 @@ const gameActions = {
 gameActions.warriorTurn();
 gameActions.mageTurn();
 gameActions.dragonTurn();
+gameActions.rogueTurn();
 console.log(gameActions.turnResults());
