3ef390d6ded659eabdbcddf52fcea25797b10327
Pedro Alves <pedro.alves.hf@gmail.com>
2022-12-01 17:47:02 -0300

    more upgrades, players with skills

--- fundamentos/bloco-8/HOF/hof-game-better.js
+++ fundamentos/bloco-8/HOF/hof-game-better.js
@@ -4,9 +4,9 @@ const mage = {
     weaponDmg: 2,
     mana: 125,
     skills: {
-        teleporte: 1, //doesn`t get hit for the round cost 50 mana / cannont use twice in a row
-        fireBall: 1, // damage normal cost 15 mana / cannont use twice in a row
-        blizzard: 1, // damage normal cost 25 mana / cannont use twice in a row
+        teleporte: "NoDmg",
+        blizzard: "Blizzard",
+        fireBall: "Fire Ball",
     },
     damage: undefined,
 };
@@ -17,22 +17,22 @@ const warrior = {
     weaponDmg: 2,
     damage: undefined,
     skills: {
-        berserker: 1, // 100% chance max hit / can use only 2 times
-        undying: 1, // Doesn`t lose HP / can use only 1 time
-        basicAttack: 1, // basic attack
-    }
+        berserker: "Max Hit",
+        undying: "Doesn`t lose HP",
+        basicAttack: "Basic Attack",
+    },
 };
 
 const rogue = {
     healthPoints: 150,
     agility: 40,
-    weaponDmg: 4,
+    weaponDmg: 3,
     damage: undefined,
     skills: {
-        vitalHit: 1, //100% chance max hit / can use only 1 time
-        vanish: 1, // Don`t receive damage from the dragon / can only use one time
-        basicAttack: 1, // basic attack
-    }
+        vitalHit: "Critical Hit",
+        vanish: "NoDmg",
+        basicAttack: "Basic Attack",
+    },
 };
 
 const dragon = {
@@ -40,9 +40,9 @@ const dragon = {
     strength: 50,
     damage: undefined,
     skills: {
-        fireBreath: "damage*2",//damage in every player / can`t use twice in a row - 2* result
-        tailHit: 1, // damage in 2/3 players
-        roar: "HP +" + 200
+        fireBreath: "damage*2",
+        tailHit: "Basic Attack",
+        roar: "HP +" + 200,
     },
 };
 
@@ -52,71 +52,171 @@ const dragonDamage = () => {
     return dragonAttack;
 };
 const dragonFireBreath = () => {
-    let dragonAttack = (Math.floor(Math.random() * ((dragon.strength*2) - 50) + 50));
+    let dragonAttack = Math.floor(
+        Math.random() * (dragon.strength * 2 - 50) + 50
+    );
     return dragonAttack;
 };
-const playerDamage =(classe, attribute) => {
-    const playerAttack = (Math.floor(Math.random() * (classe[attribute] * classe.weaponDmg - classe[attribute]) + classe[attribute]));
+const playerDamage = (classe, attribute) => {
+    const playerAttack = Math.floor(
+        Math.random() * (classe[attribute] * classe.weaponDmg - classe[attribute]) +
+        classe[attribute]
+    );
     return playerAttack;
 };
+const rogueBasicAttack = () => {
+    const rogueAttack = Math.floor(Math.random() * (rogue.agility * rogue.weaponDmg - rogue.agility) + rogue.agility);
+    return rogueAttack;
+};
+const rogueVitalHit = () => {
+    return rogueVitalDmg = rogue.agility * rogue.weaponDmg
+}
+const warriorBasicAttack = () => {
+    const warriorAttack = Math.floor(
+        Math.random() * (warrior.strength * warrior.weaponDmg - warrior.strength) +
+        warrior.strength
+    );
+    return warriorAttack;
+};
 
-const mageDamage = () => {
-    const mageC = Math.floor(Math.random() * (mage.intelligence * mage.weaponDmg - mage.intelligence) + mage.intelligence);
+const warriorBerserkerAttack = () => {
+    return (warriorAttack = warrior.strength * warrior.weaponDmg);
+};
+const mageFireballDamage = () => {
+    const mageC = Math.floor(
+        Math.random() * (mage.intelligence * mage.weaponDmg - mage.intelligence) +
+        mage.intelligence
+    );
     let mageAttack = {
         manaCost: 15,
         mageDmg: mageC,
     };
-    if(mage.mana < 15){
+    if (mage.mana < 15) {
         mageAttack = {
             manaCost: 0,
             mageDmg: 0,
         };
         return mageAttack;
-    };
+    }
     return mageAttack;
 };
+
+const mageBlizzardDamage = () => {
+    const mageC = Math.floor(
+        Math.random() * (mage.intelligence * mage.weaponDmg - mage.intelligence) +
+        mage.intelligence +
+        mage.intelligence
+    );
+    let mageAttack = {
+        manaCost: 25,
+        mageDmg: mageC,
+    };
+    if (mage.mana < 25) {
+        mageAttack = {
+            manaCost: 0,
+            mageDmg: 0,
+        };
+        return mageAttack;
+    }
+    return mageAttack;
+};
+
 const attributeCheck = (classe) => {
     let attribute;
-    if(classe.strength){
-        attribute = 'strength';
-    } else if(classe.agility){
-        attribute = 'agility';
-    } else{
-        attribute = 'intelligence';
+    if (classe.strength) {
+        attribute = "strength";
+    } else if (classe.agility) {
+        attribute = "agility";
+    } else {
+        attribute = "intelligence";
     }
-    return attribute
+    return attribute;
 };
 const gameActions = {
     playerTurn: (callback, classe) => {
         const attribute = attributeCheck(classe);
         const playerAt = callback(classe, attribute);
-        if(classe.mana){
-            if(classe.mana < 15){
+        if (classe.mana) {
+            if (classe.mana < 15) {
                 playerAt = 0;
-            }else {
+            } else {
                 classe.mana -= 15;
                 dragon.healthPoints -= playerAt;
                 classe.damage = playerAt;
+                mage.skills.fireBall = playerAt;
             }
         } else {
             dragon.healthPoints -= playerAt;
             classe.damage = playerAt;
         }
     },
+    rogueTurn: (skill) => {
+
+        if (skill === 'vitalHit') {
+            const rogueAt = rogueVitalHit();
+            dragon.healthPoints -= rogueAt;
+            rogue.damage = rogueAt;
+            rogue.skills.vitalHit = rogueAt;
+
+        }
+        if (skill === 'vanish') {
+            rogue.healthPoints += dragon.damage;
+            rogue.damage = 0;
+        }
+        if (skill === 'basicAttack') {
+            const rogueAt = rogueBasicAttack();
+            dragon.healthPoints -= rogueAt;
+            rogue.damage = rogueAt;
+            rogue.skills.basicAttack = rogueAt;
+        }
+    },
+    warriorTurn: (skill) => {
+        if (skill === "berserker") {
+            const warriorAt = warriorBerserkerAttack();
+            dragon.healthPoints -= warriorAt;
+            warrior.damage = warriorAt;
+            warrior.skills.berserker = warriorAt;
+        }
+        if (skill === "undying") {
+            warrior.healthPoints += dragon.damage;
+        }
+        if (skill === "basicAttack") {
+            const warriorAt = warriorBasicAttack();
+            dragon.healthPoints -= warriorAt;
+            warrior.damage = warriorAt;
+            warrior.skills.basicAttack = warriorAt;
+        }
+    },
+    mageTurn: (skill) => {
+        if (skill === "fireBall") {
+            let mageAt = mageFireballDamage();
+            mage.mana -= 15;
+            dragon.healthPoints -= mageAt.mageDmg;
+            mage.damage = mageAt.mageDmg;
+            mage.skills.fireBall = mageAt.mageDmg;
+        }
+        if (skill === "blizzard") {
+            let mageAt = mageBlizzardDamage();
+            mage.mana -= 25;
+            dragon.healthPoints -= mageAt.mageDmg;
+            mage.damage = mageAt.mageDmg;
+            mage.skills.blizzard = mageAt.mageDmg;
+        }
+    },
     dragonTurn: (skill) => {
-        if(skill === 'fireBreath'){
+        if (skill === "fireBreath") {
             let dragonAt = dragonFireBreath();
             warrior.healthPoints -= dragonAt;
             rogue.healthPoints -= dragonAt;
             mage.healthPoints -= dragonAt;
             dragon.damage = dragonAt;
             dragon.skills.fireBreath = dragonAt;
-        };
-        if(skill === 'roar') {
+        }
+        if (skill === "roar") {
             dragon.healthPoints += 200;
             dragon.damage = 0;
-        };
-        if(skill === 'tailHit'){
+        }
+        if (skill === "tailHit") {
             let dragonAt = dragonDamage();
             warrior.healthPoints -= dragonAt;
             rogue.healthPoints -= dragonAt;
@@ -126,9 +226,8 @@ const gameActions = {
         }
     },
 };
-
-gameActions.playerTurn(playerDamage, warrior);
-gameActions.playerTurn(playerDamage, rogue);
-gameActions.playerTurn(playerDamage, mage);
-gameActions.dragonTurn('fireBreath');
+gameActions.dragonTurn("fireBreath");
+gameActions.warriorTurn("berserker");
+gameActions.rogueTurn('vanish');
+gameActions.mageTurn("blizzard");
 console.log(battleMembers);
