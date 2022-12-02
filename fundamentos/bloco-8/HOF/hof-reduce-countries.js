4902cdb953f32a2eb5297a1a5aeff37e8bd08eaf
Pedro Alves <pedro.alves.hf@gmail.com>
2022-12-02 13:38:13 -0300

    advancing with reduce

--- fundamentos/bloco-8/HOF/hof-reduce-countries.js
+++ fundamentos/bloco-8/HOF/hof-reduce-countries.js
@@ -103,11 +103,7 @@ console.log(countA());
 
 const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
-const expectedResult = [
-{ name: 'Pedro Henrique', average: 7.8 },
-{ name: 'Miguel', average: 9.2 },
-{ name: 'Maria Clara', average: 8.8 },
-];
+
 const studentAverage = () => {
     return nameAndAverage = students.map((student, index) => (
     {
